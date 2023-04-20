dde3dd8ec82c3811a3542306fd97752d495d9e84
Javier Laguna <javier.laguna@innocv.com>
2023-04-20 16:15:09 +0200

    Initial Commit

---
+++ Slot Machine/Slot_MachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slot_MachineApp.swift
+//  Slot Machine
+//
+//  Created by Javier Laguna on 20/4/23.
+//
+
+import SwiftUI
+
+@main
+struct Slot_MachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
